8982a28d20fd0abd90a3fd3308fdf52d3821006d
amanr <amanr@Meshify-C>
2022-06-01 10:22:10 -0700

    ok

--- DashboardController/src/main/java/com/example/demo/DashboardService.java
+++ DashboardController/src/main/java/com/example/demo/DashboardService.java
@@ -1,10 +1,15 @@
 package com.example.demo;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.propertyeditors.CustomDateEditor;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,8 +23,15 @@ public class DashboardService {
 	
 	@Autowired
 	private UserDao userDao;
+	
+	@InitBinder
+    public void initBinder(WebDataBinder webDataBinder) {	
+	    SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+	    dateFormat.setLenient(false);
+	    webDataBinder.registerCustomEditor(Date.class, new CustomDateEditor(dateFormat, true));
+    }
 
-	@RequestMapping(value = "/processUser")
+	@RequestMapping(value = "/processUserData")
 	public void processUser(@RequestParam("formDto") UserDto userDto) {
 		
 		
